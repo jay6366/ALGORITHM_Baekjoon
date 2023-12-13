bf50f60cbe7ef372581f847294ea68f2fd2ce6d6
jay6366 <89118231+jay6366@users.noreply.github.com>
2023-12-14 00:00:22 +0900

    [Silver I] Title: 회의실 배정, Time: 944 ms, Memory: 82872 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.kt
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.kt
@@ -1,22 +1,23 @@
-import java.util.*
 import java.io.*
 
-fun main(){
+fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
-    val N = br.readLine().toInt()
-    val meetings = Array(N){
-        val (start, end) = br.readLine().split(' ').map{ it.toInt() }
+    val n = br.readLine().toInt()
+    val meetings = Array(n) {
+        val (start, end) = br.readLine().split(' ').map { it.toInt() }
         start to end
     }
-    
+
     meetings.sortWith(compareBy({ it.second }, { it.first }))
+
     var count = 0
     var endTime = 0
-    for (meeting in meetings){
-        if(meeting.first >= endTime){
-            endTime = meeting.second
+
+    for ((start, end) in meetings) {
+        if (start >= endTime) {
+            endTime = end
             count++
         }
     }
-    println(count)    
+    println(count)
 }

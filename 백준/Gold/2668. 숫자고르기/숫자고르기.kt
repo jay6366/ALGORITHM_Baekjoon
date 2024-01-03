d8ff706eebb48f7ac5f8cb34a5d146c0a672a669
jay6366 <89118231+jay6366@users.noreply.github.com>
2024-01-03 20:47:18 +0900

    [Gold V] Title: 숫자고르기, Time: 112 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.kt
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.kt
@@ -1,5 +1,28 @@
 import java.io.*
 
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n = br.readLine().toInt()
+    val graph = List(n + 1) { 0 }.toMutableList()
+
+    for (i in 1..n) {
+        graph[i] = br.readLine().toInt()
+    }
+
+    val visited = BooleanArray(n + 1) { false }
+    val finished = BooleanArray(n + 1) { false }
+    val result = mutableListOf<Int>()
+
+    for (x in 1..n) {
+        if (!visited[x]) dfs(x, graph, visited, finished, result)
+    }
+
+    br.close()
+
+    result.sort()
+    println(result.size)
+    result.forEach { println(it) }
+}
 fun dfs(x: Int, graph: List<Int>, visited: BooleanArray, finished: BooleanArray, result: MutableList<Int>) {
     visited[x] = true
     var y = graph[x]
@@ -13,26 +36,4 @@ fun dfs(x: Int, graph: List<Int>, visited: BooleanArray, finished: BooleanArray,
         result.add(x)
     }
     finished[x] = true
-}
-
-fun main() {
-    val input = generateSequence(::readLine).toList()
-    val n = input[0].toInt()
-    val graph = List(n + 1) { 0 }.toMutableList()
-    for (i in 1..n) {
-        graph[i] = input[i].toInt()
-    }
-    val visited = BooleanArray(n + 1) { false }
-    val finished = BooleanArray(n + 1) { false }
-    val result = mutableListOf<Int>()
-
-    for (x in 1..n) {
-        if (!visited[x]) dfs(x, graph, visited, finished, result)
-    }
-
-    result.sort()
-    println(result.size)
-    for (x in result) {
-        println(x)
-    }
 }

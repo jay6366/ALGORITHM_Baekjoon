6bd54fd8da2c6b4d7757b501899ccabf7d10e42a
jay6366 <89118231+jay6366@users.noreply.github.com>
2024-09-23 22:44:13 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.55 ms, Memory: 54.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,48 +1,57 @@
 import java.util.*;
 
 class Solution {
-    
-    class Position {
-        int x,y;
-        
-        Position(int x, int y){
+    class Point{
+        int x, y, distance; //distance 파라미터 추가 피드백
+        Point(int x, int y, int distance){ //distance 파라미터 추가 피드백
             this.x = x;
-            this.y= y;
+            this.y = y;
+            this.distance = distance;
         }
     }
     
     public int solution(int[][] maps) {
-        int mapHeight = maps.length;
-        int mapWidth = maps[0].length;
-        Queue<Position> queue = new LinkedList<>();
-        int[][] count = new int [mapHeight][mapWidth];
-        boolean[][] visited = new boolean [mapHeight][mapWidth];
+        int answer = 0;
+        int level = 0;
         
-        count[0][0] = 1;
+        int n = maps.length; //배열 행과 열 가져오는 방법 꼭 익히기
+        int m = maps[0].length; //배열 행과 열 가져오는 방법 꼭 익히기
+        
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = { 0, 1, 0, -1};
+        
+        Queue<Point> queue = new LinkedList<>();
+        answer++;
+        queue.offer(new Point(0,0,1)); //파라미터 추가로 인한 초기화 변경 : 시작점은 (0,0) 위치, 거리는 1
+        
+        boolean[][] visited = new boolean[n][m]; //boolean visited 배열 초기화 잊지말자
         visited[0][0] = true;
         
-        queue.offer(new Position(0,0));
-        
         while(!queue.isEmpty()){
-            Position current = queue.poll();
+            //int len = queue.size(); 이 문제에서 len은 필요 없었음
             
-            int currentCount = count[current.y][current.x];
-                      
-            final int[][] moving = {{0,-1}, {0,1},{-1,0},{1,0}};
-            for(int i=0; i<moving.length; i++){
-                Position moved = new Position(current.x + moving[i][0], current.y + moving[i][1]);
-                if(moved.x >= 0 && moved.x <mapWidth && moved.y >= 0 && moved.y < mapHeight 
-                                && !visited[moved.y][moved.x] && maps[moved.y][moved.x]==1){
-                    count[moved.y][moved.x] = currentCount + 1;
-                    visited[moved.y][moved.x] = true;
-                    queue.offer(moved);
+            //for(int i=0; i<len; i++){
+                
+                Point tmp = queue.poll();
+                
+                if(tmp.x == n-1 && tmp.y == m-1){
+                    return tmp.distance;
+                } //목적지에 도달한 경우 distance 반환
+                
+                for(int j=0; j<4; j++){
+                    int nx = tmp.x + dx[j];
+                    int ny = tmp.y + dy[j];
+                    if(nx>=0 && nx<n && ny>=0 && ny<m && maps[nx][ny]!=0 && !visited[nx][ny]){
+                        //visited배열 조건에 추가 + out of index 조심
+                        visited[nx][ny] = true;
+                        queue.offer(new Point(nx,ny,tmp.distance+1));
+                    }
                 }
-            }            
+
+            //}         
         }
-        int answer = count[mapHeight-1][mapWidth-1];
-        if(answer == 0) return -1;
         
-              
-        return answer;
+        //도달할 수 없는 경우
+        return -1;
     }
 }

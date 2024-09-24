077e0d5d0c2cffc8dc8588f3ae43191de0d1cea0
jay6366 <89118231+jay6366@users.noreply.github.com>
2024-09-24 20:04:01 +0900

    [level 2] Title: 전화번호 목록, Time: 295.81 ms, Memory: 253 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
 
 ### 성능 요약
 
-메모리: 132 MB, 시간: 208.78 ms
+메모리: 253 MB, 시간: 295.81 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 30일 22:02:48
+2024년 09월 24일 20:03:56
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,24 +1,21 @@
 import java.util.*;
-
 class Solution {
-    public Boolean solution(String[] phone_book) {
-        Boolean answer = true;
+    public boolean solution(String[] phone_book) {       
+        HashMap<String,Integer> map = new HashMap<>();
         
-        
-        HashMap<String,Boolean> map = new HashMap<String,Boolean>();
-        for(String book : phone_book){
-            map.put(book,true);
+        for(String number : phone_book){
+            map.put(number,map.getOrDefault(number,0)+1);
         }
         
-        for(String book : phone_book){
-            for(int i=0; i<book.length(); i++){
-                String prefix = book.substring(0,i);
+        for(String number : phone_book){
+            for(int i=0; i<number.length();i++){
+                String prefix = number.substring(0,i); //생각못한부분
                 if(map.containsKey(prefix)){
                     return false;
                 }
             }
         }
-                
+
         return true;
     }
 }

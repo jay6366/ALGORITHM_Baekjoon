0742afdd094336020cf6afa21ca855819b42697b
jay6366 <89118231+jay6366@users.noreply.github.com>
2024-09-24 22:18:15 +0900

    [level 3] Title: 여행경로, Time: 0.49 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    Map<String,PriorityQueue<String>> map = new HashMap<>();
+    List<String> route = new ArrayList<>();
+    
+    public void DFS(String airport){
+        while(map.containsKey(airport)&&!map.get(airport).isEmpty()){
+            DFS(map.get(airport).poll());            
+        }
+        route.add(0,airport);
+    }
+    
+    public String[] solution(String[][] tickets) {
+        for(String[] ticket : tickets){
+            map.putIfAbsent(ticket[0],new PriorityQueue<String>());
+            map.get(ticket[0]).offer(ticket[1]);
+        }
+        
+        DFS("ICN");
+        return route.toArray(new String[0]);
+
+    }
+}

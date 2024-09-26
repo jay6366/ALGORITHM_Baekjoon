6f962cb41f17a7aac9ee26b6c473e91327fdbf6a
jay6366 <89118231+jay6366@users.noreply.github.com>
2024-09-26 21:34:24 +0900

    [level 2] Title: 구명보트, Time: 10.02 ms, Memory: 56.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+        int answer = 0;
+        int right = people.length-1;
+        int left = 0;
+        while(left<=right){
+            if(people[left]+people[right]<=limit){
+                left++;
+            }
+            right--;
+            answer++;
+        }
+        return answer;
+    }
+}

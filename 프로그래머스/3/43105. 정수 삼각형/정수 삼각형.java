3480500773df7b9b967434e1aa3204e98ada421c
jay6366 <89118231+jay6366@users.noreply.github.com>
2024-10-24 20:17:40 +0900

    [level 3] Title: 정수 삼각형, Time: 8.91 ms, Memory: 61 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,22 +1,51 @@
-import java.lang.*;
-
+import java.util.*;
 class Solution {
     public int solution(int[][] triangle) {
-        int answer = 0;
+        int answer = Integer.MIN_VALUE;
+        
+        int[][]dp = new int[triangle.length][triangle.length];
+        
+        dp[0][0] = triangle[0][0];
+        
         for(int i=1; i<triangle.length; i++){
-            for(int j=0; j<triangle[i].length; j++){
-                if(j==0){
-                    triangle[i][j] = triangle[i][j]+triangle[i-1][j];
-                } else if (i==j){
-                    triangle[i][j] = triangle[i][j]+triangle[i-1][j-1];
-                } else {
-                    int right = triangle[i][j]+triangle[i-1][j-1];
-                    int left = triangle[i][j]+triangle[i-1][j];
-                    triangle[i][j] = Math.max(right,left);
-                }
-                answer = Math.max(answer,triangle[i][j]);
+            
+            //가장 왼쪽
+            dp[i][0] = dp[i-1][0] + triangle[i][0];
+            
+            //가장 오른쪽
+            dp[i][i] = dp[i-1][i-1] + triangle[i][i];
+            
+            //가운데
+            for(int j=1; j<i; j++){
+                dp[i][j] = Math.max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j];
             }
         }
+        
+        for(int x : dp[triangle.length-1]){
+            answer = Math.max(answer,x);
+        }
+        
+        
+//         List<Set<Integer>> dp = new ArrayList<>();
+        
+//         for(int i=0; i<triangle.length; i++){
+//             dp.add(new HashSet<>());           
+//         }
+//         dp.get(0).add(triangle[0][0]);
+        
+        
+//         for(int i=1; i<triangle.length; i++){
+//             for(int j=0; j<triangle[i].length; j++){
+//                 dp.get(i) = dp.get(i-1).add()                
+//             }
+
+//         }
+        
+//         for(int y : dp.get(triangle.length-1)){
+//             answer = Math.max(y,answer);
+//         }
+        
         return answer;
+              
     }
 }

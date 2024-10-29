8f68972e54bde97fa0296110ebb990687a580e9b
jay6366 <89118231+jay6366@users.noreply.github.com>
2024-10-29 20:36:12 +0900

    [level 2] Title: 더 맵게, Time: 1569.12 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,29 +1,33 @@
 import java.util.*;
 class Solution {
     public int solution(int[] scoville, int K) {
-        Arrays.sort(scoville);
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
         
-        for(int i=0; i<scoville.length; i++){
-            minHeap.add(scoville[i]);
-        }
-        int count=0;
-        while(!minHeap.isEmpty()){
-            int num1 = minHeap.poll();
-            if(num1 >= K){
-                break;
-            }
-            if(!minHeap.isEmpty()){
-                int num2 = minHeap.poll();  
-                int num3 = num1 + (num2 * 2);
-                minHeap.add(num3); 
-                count++;
-            } else{
-                return -1;
-            }
-       
+        //최소힙을 구현하는 기본 클래스 PriortyQueue, 가장 작은 요소가 큐의 맨 앞에 위치함
+        //PriorityQueue는 삽입과 삭제 연산이 O(log N) 복잡도를 가짐
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        
+        //모든 음식의 스코필 지수를 힙에 추가
+        for(int score : scoville){
+            heap.offer(score);
         }
         
-        return count;
+        int mixCount = 0;
+        
+        // 가장 작은 두 지수가 있고,가장 작은 지수가 K이하일 때 계속 반복
+        while(heap.size()>1 && heap.peek()<K){
+            int leastSpicy = heap.poll();
+            int secondLeastSpicy = heap.poll();
+            
+            int newScoville = leastSpicy + (secondLeastSpicy * 2);
+            heap.offer(newScoville);
+            mixCount++;
+        }
+        
+        if(heap.peek()<K){
+            return -1;
+        }
+        
+        
+        return mixCount;
     }
 }

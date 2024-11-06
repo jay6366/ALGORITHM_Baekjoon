130a4911b8c1fcac2cd9924e13e07b163cb73c1d
jay6366 <89118231+jay6366@users.noreply.github.com>
2024-11-06 20:31:41 +0900

    [level 3] Title: 네트워크, Time: 0.18 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,17 +1,9 @@
 class Solution {
     boolean[] visited;
-    
-    public void DFS(int number, int[][] computers){
-        visited[number] = true;
-        for(int i=0; i<computers.length; i++){
-            if(computers[number][i]==1 && !visited[i]){
-                DFS(i,computers);
-            }
-        }
-    }
-    public int solution(int n, int[][] computers) {       
+    public int solution(int n, int[][] computers) {
         visited = new boolean[n];
         int networkCount = 0;
+        
         for(int i=0; i<n; i++){
             if(!visited[i]){
                 DFS(i,computers);
@@ -19,5 +11,14 @@ class Solution {
             }
         }
         return networkCount;
+    }
+    public void DFS(int index, int[][] computers){
+        visited[index] = true;
+        
+        for(int i=0; i<computers.length; i++){
+            if(computers[index][i]==1 && !visited[i]){
+                DFS(i,computers);
+            }
+        }
     }
 }

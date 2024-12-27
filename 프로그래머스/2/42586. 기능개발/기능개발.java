52af585d0e090c9829bcccd172bcc7bb96953eea
jay6366 <89118231+jay6366@users.noreply.github.com>
2024-12-27 22:29:44 +0900

    [level 2] Title: 기능개발, Time: 3.66 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,21 +2,20 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         Queue<Integer> queue = new LinkedList<>();
-        List<Integer> result = new ArrayList<>(); 
+        List<Integer> result = new ArrayList<>();
         
-        //각 기능 별 완료까지 소요도는 일 수 계산
         for(int i=0; i<progresses.length; i++){
-            int days= (100-progresses[i])/speeds[i]
-                      +((100-progresses[i])%speeds[i] > 0 ? 1 : 0);
-            queue.offer(days);
+            int remainingProgress = 100 - progresses[i];
+            int remainingDays = remainingProgress / speeds[i] + 
+                (remainingProgress % speeds[i] > 0 ? 1 : 0);
+            queue.offer(remainingDays);
         }
         
-        //배포로직
         while(!queue.isEmpty()){
-            int count = 1;
             int day = queue.poll();
+            int count = 1;
             
-            while(!queue.isEmpty() && queue.peek()<=day){
+            while(!queue.isEmpty() && queue.peek() <= day){
                 count++;
                 queue.poll();
             }

a01501b83c54de2419381d2334f9205b6b49c4d2
jay6366 <89118231+jay6366@users.noreply.github.com>
2024-12-29 13:54:31 +0900

    [level 2] Title: 피로도, Time: 3.12 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,18 +1,17 @@
 import java.util.*;
 class Solution {
     boolean[] visited;
-    int answer = 0;
-    int count;
-    
+    int count=0;
+    int answer=0;
     void DFS(int depth, int remain, int[][] dungeons, int n){
-        //if(depth==n){ 던전을 모두 탐험할 수 있는 없는 경우에도 탐색을 종료할 수 있게 해야함
+        
         answer = Math.max(answer,count);
-        //}
+        
         for(int i=0; i<n; i++){
-            if(!visited[i] && remain >= dungeons[i][0]){
+            if(!visited[i]&&remain>= dungeons[i][0]){
                 visited[i] = true;
                 count++;
-                DFS(depth+1,remain-dungeons[i][1], dungeons, n);
+                DFS(depth+1,remain-dungeons[i][1],dungeons,n);
                 count--;
                 visited[i] = false;
             }
@@ -21,9 +20,8 @@ class Solution {
     
     public int solution(int k, int[][] dungeons) {
         int n = dungeons.length;
-        count = 0;
         visited = new boolean[n];
-        DFS(0, k, dungeons, n);
+        DFS(0,k,dungeons,n);
         return answer;
     }
 }

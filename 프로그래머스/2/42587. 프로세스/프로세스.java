bd82951510f6d1cf47b4a37db90cb11881fca408
jay6366 <89118231+jay6366@users.noreply.github.com>
2024-12-30 19:42:14 +0900

    [level 2] Title: 프로세스, Time: 2.60 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -6,35 +6,32 @@ class Point{
         this.priority = priority;
     }
 }
-
 class Solution {
     public int solution(int[] priorities, int location) {
+        int count = 0;
         Queue<Point> queue = new LinkedList<>();
-        
         for(int i=0; i<priorities.length; i++){
             queue.offer(new Point(i,priorities[i]));
         }
         
-        
-        int count = 0;
-        while(!queue.isEmpty()){ //for문이 아닌 queue가 빌 때까지 반복문 실행
+        while(!queue.isEmpty()){
             Point tmp = queue.poll();
-            boolean isMax = true; //해당 변수 생각못함
-            for(Point p : queue){ //queue도 반복문 돌릴 수 있음
-                if(tmp.priority<p.priority){
+            boolean isMax = true;
+            for(Point po : queue){
+                if(tmp.priority<po.priority){
                     isMax = false;
-                }               
+                     break;
+                }
             }
-            if(!isMax){ //이렇게 해야 큐에서 요소를 제거할 때마다 각각의 현재 큐에 맞는 로직 적용 가능
-                queue.offer(tmp);
-            } else {
-                //실행이 될 때 count해야함
+            if(isMax){
                 count++;
                 if(tmp.index == location){
                     break;
                 }
+            } else {
+                queue.offer(tmp);
             }
-        }          
+        }
         return count;
     }
 }

51aa269ea50a1fe62c6b17dcf8c5fae2ab08ce71
jay6366 <89118231+jay6366@users.noreply.github.com>
2024-12-31 20:32:56 +0900

    [level 2] Title: 큰 수 만들기, Time: 57.44 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,27 +1,23 @@
 import java.util.*;
-
 class Solution {
-    //가장 큰 숫자를 구하는 것이 포인트임, 조합에 초점을 맞췄으면 안됨
-    //==>핵심파악이 곧 사용해야할 자료구조에 대한 아이디어로 이어짐 : 스택
     public String solution(String number, int k) {
         Stack<Character> stack = new Stack<>();
         for(char x : number.toCharArray()){
-            while(!stack.isEmpty() && k>0 && x>stack.peek()){
+            while(!stack.isEmpty()&& k>0 && x>stack.peek()){
                 stack.pop();
                 k--;
             }
             stack.push(x);
         }
-        StringBuilder sb = new StringBuilder();
         
-        while(stack.size()>number.length()-k){
+        StringBuilder sb = new StringBuilder();
+        while(stack.size() > number.length() - k){
             stack.pop();
         }
         
-        while(!stack.isEmpty()){
-            sb.append(stack.pop());
+        for(char x : stack){
+            sb.append(x);           
         }
-            
-        return sb.reverse().toString();
+        return sb.toString();
     }
 }

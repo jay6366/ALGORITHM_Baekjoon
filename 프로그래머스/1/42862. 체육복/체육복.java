62ead53a4e8075cf139053d1ab9b72fc52b98e69
jay6366 <89118231+jay6366@users.noreply.github.com>
2025-01-01 22:06:39 +0900

    [level 1] Title: 체육복, Time: 0.52 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,3 +1,4 @@
+import java.util.*;
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
@@ -12,26 +13,21 @@ class Solution {
             clothes[i]++;
         }
         
-        // for(int i : lost){
-        //     if(i>1&&clothes[i-1]>1){
-        //         clothes[i-1]--;
-        //         clothes[i]++;
-        //     } else if(i+1<=n&&clothes[i+1]>1){
-        //         clothes[i+1]--;
-        //         clothes[i]++;
-        //     }
-        // }
-        for (int i = 1; i <= n; i++) {
-            if (clothes[i] == 0) { // 체육복이 없는 경우
-                if (i > 1 && clothes[i - 1] > 1) { // 왼쪽 학생 확인
-                    clothes[i]++;
-                    clothes[i - 1]--;
-                } else if (i < n && clothes[i + 1] > 1) { // 오른쪽 학생 확인
-                    clothes[i]++;
-                    clothes[i + 1]--;
-                }
+        Arrays.sort(lost);
+        
+         for(int i : lost){
+             if(clothes[i]==0){
+                if(i>1&&clothes[i-1]>1){
+                 clothes[i-1]--;
+                 clothes[i]++;
+             } else if(i+1<=n&&clothes[i+1]>1){
+                 clothes[i+1]--;
+                 clothes[i]++;
+             }              
             }
-        }
+
+         }
+
 
         for(int i=1; i<=n; i++){
             if(clothes[i]>=1){

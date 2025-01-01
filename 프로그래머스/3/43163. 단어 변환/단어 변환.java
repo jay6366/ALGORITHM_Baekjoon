80aaf8ad4ba3eaf95c5322519bc9807c08a80c3a
jay6366 <89118231+jay6366@users.noreply.github.com>
2025-01-01 21:07:06 +0900

    [level 3] Title: 단어 변환, Time: 0.95 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,56 +1,55 @@
 import java.util.*;
 class Word{
-    String begin;
+    String beginWord;
     int step;
     Word(String begin, int step){
-        this.begin = begin;
+        this.beginWord = begin;
         this.step = step;
     }
 }
 
 class Solution {
-    boolean isOneLetterDifferent(String word1, String word2){
+    public boolean isOneLetterDifferent(String current, String next){
         int count = 0;
-        for(int i=0; i<word1.length();i++){
-            if(word1.charAt(i)!=word2.charAt(i)){
+        for(int i=0; i<current.length(); i++){
+            if(current.charAt(i)!=next.charAt(i)){
                 count++;
             }
         }
-        if(count == 1){
+        if(count==1){
             return true;
         }
         return false;
     }
-    
     public int solution(String begin, String target, String[] words) {
-        List<String> wordList = Arrays.asList(words); //List변환
+        List<String> wordList = Arrays.asList(words);
         if(!wordList.contains(target)){
             return 0;
         }
         
         Queue<Word> queue = new LinkedList<>();
-        Set<String> visited = new HashSet<>(); //HashSet
-        
         queue.offer(new Word(begin,0));
+        
+        Set<String> visited = new HashSet<>();
         visited.add(begin);
         
         while(!queue.isEmpty()){
-            Word tmp = queue.poll();
-            String currentWord = tmp.begin;
-            int currentSteps = tmp.step;
+            Word cur = queue.poll();
+            String currentWord = cur.beginWord;
+            int steps = cur.step;
             
             if(currentWord.equals(target)){
-                return currentSteps;
+                return steps;
             }
             
-            for(String word : wordList){ //배열, List 모두 할 수 있지?
-                if(!visited.contains(word)&&isOneLetterDifferent(tmp.begin,word)){
-                    queue.offer(new Word(word,currentSteps+1));
+            for(String word : wordList){
+                if(!visited.contains(word)&&isOneLetterDifferent(currentWord,word)){
+                    queue.offer(new Word(word,steps+1));
                     visited.add(word);
                 }
-            }
+            }           
         }
         return 0;
+        
     }
 }
-                                
